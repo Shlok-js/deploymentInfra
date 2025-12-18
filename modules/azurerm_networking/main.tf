eef93d27a4ae9f7d303b5638adf74b2b725b8b23
Shlok Jambusariya <shlok050301@gmail.com>
2025-12-19 01:45:02 +0530

    new changes feature

--- modules/azurerm_networking/main.tf
+++ modules/azurerm_networking/main.tf
@@ -65,8 +65,7 @@ resource "azurerm_bastion_host" "bastion" {
   name                = "${each.key}-bastion"
   location            = var.vnets_subnets[each.key].location
   resource_group_name = var.vnets_subnets[each.key].resource_group_name
-
-
+  
   ip_configuration {
     name                 = "configuration"
     subnet_id            = local.snet_ids[each.key].AzureBastionSubnet
