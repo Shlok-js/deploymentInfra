0f190cdde9fbb00aa7abe4478b3f4804fc4d198e
Shlok Jambusariya <shlok050301@gmail.com>
2025-12-20 01:49:12 +0530

    pipeline run6

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,8 @@
- terraform {
-    backend "azurerm" {
-        resource_group_name   = "rg-pompu"
-        storage_account_name  = "pompustorage"
-        container_name        = "tfstate"
-        key                   = "dev.terraform.tfstate"
-    }
- }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-backend"
+    storage_account_name = "pompu"
+    container_name       = "tfstate"
+    key                  = "deploymentInfra.tfstate"
+  }
+}
