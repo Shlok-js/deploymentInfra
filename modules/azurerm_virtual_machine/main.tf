619f94410f127a008f883cf1ca6b35c2f138e85b
Shlok Jambusariya <shlok050301@gmail.com>
2025-12-20 00:51:54 +0530

    pipeline run4

--- modules/azurerm_virtual_machine/main.tf
+++ modules/azurerm_virtual_machine/main.tf
@@ -23,6 +23,7 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+
 resource "azurerm_network_security_group" "nsg" {
   for_each            = var.vms
   name                = "${each.key}-nsg"
