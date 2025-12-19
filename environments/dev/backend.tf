0b0c0a643a2c429bdadd7e30a569c7051553cf80
Shlok Jambusariya <shlok050301@gmail.com>
2025-12-20 01:52:44 +0530

    pipeline run7

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -3,6 +3,7 @@ terraform {
     resource_group_name  = "rg-tf-backend"
     storage_account_name = "pompu"
     container_name       = "tfstate"
+ 
     key                  = "deploymentInfra.tfstate"
   }
 }
