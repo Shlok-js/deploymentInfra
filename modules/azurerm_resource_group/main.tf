708ffea526b39cbd6e067f72b574392f5f63bf28
Shlok Jambusariya <shlok050301@gmail.com>
2025-12-20 16:23:11 +0530

    test run

--- modules/azurerm_resource_group/main.tf
+++ modules/azurerm_resource_group/main.tf
@@ -2,8 +2,10 @@ resource "azurerm_resource_group" "rg" {
   for_each = var.rgs
 
   name     = each.key
+
   location = each.value.location
 }
+
 
 output "rg_ids" {
   value = { for key, value in azurerm_resource_group.rg: value.name => value.id }
