78bb4d3aa50dce8e5135e955712df73c3ececb45
Shlok Jambusariya <shlok050301@gmail.com>
2025-12-27 00:26:33 +0530

    run 26 dec

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-tf-backend"
+    resource_group_name  = "bootstrap"
     storage_account_name = "pompya"
     container_name       = "tfstate"
     key                  = "deploymentInfra.tfstate"
