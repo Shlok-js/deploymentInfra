17f2ede938693f740b0783e707ea4a16052a3292
Shlok Jambusariya <shlok050301@gmail.com>
2025-12-27 07:11:29 +0530

    changes in the backend config

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -3,6 +3,7 @@ terraform {
     resource_group_name  = "bootstrap"
     storage_account_name = "pompya"
     container_name       = "tfstate"
+    
     key                  = "deploymentInfra.tfstate"
   }
 }
