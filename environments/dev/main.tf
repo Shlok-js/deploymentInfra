3c70ab7db161a0bdf097d8c5ce8e517c1598b2e0
Shlok Jambusariya <shlok050301@gmail.com>
2025-12-29 01:17:43 +0530

    Fix VM xomment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,12 +10,12 @@ module "networking" {
   vnets_subnets = var.vnets_subnets
 }
 
-module "vms" {
-  depends_on      = [module.azurerm_resource_group, module.networking]
-  source          = "../../modules/azurerm_virtual_machine"
-  vms             = var.vms
-  vnet_subnet_ids = module.networking.vnet_subnet_ids
-}
+# module "vms" {
+#   depends_on      = [module.azurerm_resource_group, module.networking]
+#   source          = "../../modules/azurerm_virtual_machine"
+#   vms             = var.vms
+#   vnet_subnet_ids = module.networking.vnet_subnet_ids
+# }
 
 # module "loadbalancers" {
 #   depends_on    = [module.rgs, module.networking, module.vms]
