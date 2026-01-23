21a9023592eb1f75ecb06e0300157ae3c994e5e8
Shlok Jambusariya <shlok050301@gmail.com>
2026-01-23 16:28:45 +0530

    testing new sub

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,11 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-dont-delete"
+
     storage_account_name = "bhagadbilla" 
+
     container_name       = "tfstate"
+    
     key = "deploymentInfra.tfstate"
   }
 }
